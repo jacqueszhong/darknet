2f96748bdc0189ef99fb83b5012eb626a3c32caa
Jacques Zhong <jacques.zhong@outlook.fr>
2019-03-25 12:16:43 +0100

    chair door cfg

---
+++ launch_test.sh
@@ -0,0 +1,8 @@
+
+
+#21/03
+#Environ 500 itérations, avec base complète, transfert de tous les poids, chair=56 et door=0.
+./darknet detector demo cfg_transfer80/obj.data cfg_transfer80/yolov3-tiny.cfg models/2103_test_500it.weights -c 1
+
+
+#./darknet detector demo cfg/coco.data cfg/yolov3-tiny.cfg ../WEIGHTS/yolov3-tiny.weights -c 1
